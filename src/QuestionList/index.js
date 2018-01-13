99ca9a2fd1a87e21c86115a646cf61b67b2699c3
Megh Parikh <meghprkh@gmail.com>
2018-01-14 02:21:35 +0530

    Store questions list in window.questions

--- src/QuestionList/index.js
+++ src/QuestionList/index.js
@@ -1,14 +1,17 @@
 import Link from "../Link";
 import dataUIComponent from "../dataUIComponent";
 
-const QuestionList = ({ data }) => (
-  <ul>
-    {data.map(question => (
-      <li>
-        <Link to={`/question/${question.id}/`}>{question.title}</Link>
-      </li>
-    ))}
-  </ul>
-);
+const QuestionList = ({ data }) => {
+  window.questions = data;
+  return (
+    <ul>
+      {data.map(question => (
+        <li>
+          <Link to={`/question/${question.id}/`}>{question.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default dataUIComponent(QuestionList, "Questions", "/questions");
